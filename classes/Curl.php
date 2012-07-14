fa769ed035b5a0a6bce50d4b7afdb37ceb05b8b0
korenskoy <anton@korenskoy.ru>
2012-07-15 00:16:26 +1100

    Добавлена поддержка cookies

--- classes/Curl.php
+++ classes/Curl.php
@@ -10,34 +10,50 @@
  */
 class Curl {
 
-	/** 	
+	/**
 	 * Curl Handle
-	 * @var 
+	 * @var
 	 */
 	protected $ch;
-	
+
 	/**
 	 * Fetch received headers
 	 * @var boolean
 	 */
 	protected $fetch_headers = false;
-	
+
 	/**
 	 * Received headers after execution
-	 * @var string 
+	 * @var string
 	 */
 	protected $headers_received;
+
+    /**
+     * Cookie file.
+     *
+     * @var string
+     */
+    protected $cookie_file;
 
 	/**
 	 * Initializes curl handler
 	 */
-	public function __construct() {
-		
+	public function __construct($use_cookie = false) {
+
 		// Initializes Curl handler
 		$this->ch = curl_init();
-		
+
 		// Result will be returned not outputed
 		$this->setopt(CURLOPT_RETURNTRANSFER, true);
+
+        if ($use_cookie == true)
+        {
+            $this->cookie_file = __DIR__ . '/cookies.txt';
+            // ensure cookie is writable by attempting to open it up
+            $this->open_cookie();
+            $this->setopt(CURLOPT_COOKIEFILE, $this->cookie_file);
+            $this->setopt(CURLOPT_COOKIEJAR, $this->cookie_file);
+        }
 	}
 
 	// @TODO Jāizveido iespēja rezultātu uzreiz saglabāt failā uz fopen handle
@@ -46,7 +62,7 @@ class Curl {
 	 * Set curl parameter
 	 * @param integer $option Curl constant
 	 * @see http://php.net/manual/en/function.curl-setopt.php
-	 * @param mixed $value 
+	 * @param mixed $value
 	 */
 	public function setopt($option, $value) {
 
@@ -55,10 +71,10 @@ class Curl {
 
 	/**
 	 * Sets data request method
-	 * @param string $method 
+	 * @param string $method
 	 */
 	public function set_method($method) {
-		
+
 		switch ($method) {
 			case 'POST' : curl_setopt($this->ch, CURLOPT_POST, true);
 				break;
@@ -71,109 +87,140 @@ class Curl {
 	/**
 	 * Set post parameter string.
 	 * Data will be sent as post.
-	 * @param string $post_string 
+	 * @param string $post_string
 	 */
 	public function set_post_string($post_string) {
-		
+
 		$this->setopt(CURLOPT_POSTFIELDS, $post_string);
 	}
 
 	/**
 	 * Set execution url. This can be also set in exec method.
-	 * @param string $url 
+	 * @param string $url
 	 */
 	public function set_url($url) {
-		
+
 		curl_setopt($this->ch, CURLOPT_URL, $url);
 	}
 
 	/**
 	 * Execute request
-	 * @param string $url 
+	 * @param string $url
 	 * @return string
 	 */
 	public function exec($url = null) {
-		
+
 		// sets execution url if it is supplied
 		if ($url !== null) {
 			$this->set_url($url);
 		}
-		
+
 		// Received headers must be retrieved
 		if($this->fetch_headers) {
-			
+
 			// set curl to return headers
 			$this->setopt(CURLOPT_HEADER, true);
-			
+
 			// Executes the request
 			$result = curl_exec($this->ch);
-			
+
 			// set curl to NOT return headers
 			$this->setopt(CURLOPT_HEADER, false);
-			
+
 			// Seperate header from body
 			echo $result;
 			$pos = strpos("\n\n", $result);
-			
+
 			if($pos === false) {
 				throw new Exception('No headers received!', 2);
 			}
-			
+
 			$this->headers_received = substr($result, 0, $pos);
-			
+
 			$result = substr($result, $pos+4, strlen($result)-$pos-4);
-			
+
 		}
 		else {
 			// Executes the request
 			$result = curl_exec($this->ch);
 		}
 
-		
+
 
 		return $result;
 	}
 
 	/**
 	 * Atgriež headerus kādi tika nosūtīti uz serveri
-	 * @return string 
+	 * @return string
 	 */
 	public function get_headers_sent() {
-		
+
 		return curl_getinfo($this->ch, CURLINFO_HEADER_OUT);
 	}
-	
+
 	/**
 	 * Set to fetch received headers.
 	 * This must be set before executing request via exec method
 	 * afterwars use get_headers_received method
 	 */
 	public function set_fetch_headers() {
-		
+
 		$this->fetch_headers = true;
 	}
-	
+
 	public function get_headers_receved() {
 		return $this->headers_received;
 	}
-	
+
 	/**
 	 * Returns curl handle.
-	 * @return type 
+	 * @return type
 	 */
 	public function get_handle() {
 		return $this->ch;
 	}
-	
+
 	/**
 	 * File download
 	 * Save results into a file
-	 * @param string $filename 
+	 * @param string $filename
 	 */
 	public function save_into_file($filename) {
-		
+
 		$file_handle = fopen($filename,'c+x+');
-		
+
 		$this->setopt(CURLOPT_FILE, $file_handle);
 	}
+
+
+    /**
+     * open_cookie
+     *
+     * @access protected
+     * @return void
+     */
+    protected function open_cookie() {
+        // ensure file is writable
+        if (file_exists($this->cookie_file))
+        {
+            if (posix_access($this->cookie_file, POSIX_W_OK) === false)
+            {
+                throw new Exception('File *' . ($this->cookie_file) . '* must be writable ' . 'for cookie storage.');
+            }
+        }
+        // ensure file directory is writable
+        else
+        {
+            $dir = dirname($this->cookie_file);
+            if (is_writable($dir) === false)
+            {
+                throw new Exception('Path *' . ($dir) . '* must be writable for cookie ' . 'storage.');
+            }
+
+            // open file
+            $resource = fopen($this->cookie_file, 'w');
+            fclose($resource);
+        }
+    }
 }
